c3d1145f051de43581ed1c519c102693f940de7a
Sabrina Bilmak <sabrinabilmak@imac-sabrina-1.home>
2022-10-02 21:19:27 +0200

    add handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/zhashkevych/todo-app"
+	"github.com/zhashkevych/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf(" problems with runing: %s", err.Error())
 	}
 }
