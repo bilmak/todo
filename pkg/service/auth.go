c88850645e8b0777e05e5c77a019cf5b29a9c237
Sabrina Bilmak <sabrinabilmak@imac-sabrina-1.home>
2022-10-07 11:24:15 +0200

    5

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,14 +2,25 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
+	"time"
 
 	todo "github.com/bilmak/todo-app/pkg"
 	"github.com/bilmak/todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "sabrina1234"
+const (
+	salt       = "sabrina1234"
+	singingKey = "123rfv"
+	tokenTTL   = 12 * time.Hour
+)
 
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json: "user_id"`
+}
 type AuthService struct {
 	repo repository.Authorization
 }
@@ -20,8 +31,27 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
-	
+
 	return s.repo.CreateUser(user)
+
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(singingKey))
 
 }
 
@@ -30,4 +60,22 @@ func generatePasswordHash(password string) string {
 	hash.Write([]byte(password))
 
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(singingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token climes are not of type")
+	}
+
+	return claims.UserId, nil
 }
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -9,6 +9,8 @@ import (
 
 type Authorization interface{
 	CreateUser(user todo.User) (int, error)
+	GenerateToken (username, password string) (string, error)
+	ParseToken(token string)(int, error)
 
 }
 
